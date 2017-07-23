e7220fdfc79bf17e65c6efc58cf7b192dccb7d3f
Sam Ojling <s.ojling@gmail.com>
2017-07-23 12:07:32 -0700

    trying to solve path issue

--- src/pages/my-second-gatsby-page.js
+++ src/pages/my-second-gatsby-page.js
@@ -4,5 +4,5 @@ import Link from 'gatsby-link'
 export default () =>
   <div>
     <p>Hello world from my second Gatsby page</p>
-    <Link to="/dev">back home</Link>
+    <Link to="/">back home</Link>
   </div>
