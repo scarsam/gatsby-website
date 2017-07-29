05e0f8a7481e96e2e3d7fdaffee58acd288f4916
Sam Ojling <s.ojling@gmail.com>
2017-07-29 12:33:02 -0700

    Added scss to the site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import '../styles/typography.scss'
 
 export default () => 
-  <div style={{ color: `tomato` }}>
-    <h1>Hello world, this is Sam!</h1>
-    <p>What a world.</p>
+  <div>
+    <h1>This will be my blog</h1>
+    <p>work in progress</p>
     <img src="http://lorempixel.com/400/200/" />
     <br />
     <Link to="/my-second-gatsby-page/">Link</Link>
