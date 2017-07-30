4de064e437b074637cc17bc4b53bf196952913fd
Sam Ojling <s.ojling@gmail.com>
2017-07-30 13:20:37 -0700

    Pushing my changes again

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
+import 'normalize.css';
 import React from 'react'
 import Link from 'gatsby-link'
+import '../styles/styles.scss'
 
-export default () => 
-  <div style={{ color: `tomato` }}>
-    <h1>Hello world, this is Sam!</h1>
-    <p>What a world.</p>
+export default () =>
+  <main>
+    <h1>This will be my blog</h1>
+    <p>work in progress</p>
     <img src="http://lorempixel.com/400/200/" />
     <br />
     <Link to="/my-second-gatsby-page/">Link</Link>
-  </div>
+  </main>
